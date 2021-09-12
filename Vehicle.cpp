08e6b91e16f642295465929e39ef5507e8d2027c
nandakrishnan nair <nandu.99.nair@gmail.com>
2021-09-12 09:17:56 +0530

    Added menu driven functionality

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -3,29 +3,51 @@
 #include<string.h>
 using namespace std;
 
-class VehicleType
+class ParkingArea
 {
     private:
+    const int type1limit=10;
+    const int type2limit=10;
+    const int type3limit=10;
+    static int type1cnt;
+    static int type2cnt;
+    static int type3cnt;
+};
+class VehicleType
+{
+    protected:
+        int vtypeid;
+        int priceperhour; 
         string type;
     public:
         VehicleType()
         {
+            int vtypeid=0;
             type="vehicle";
+            priceperhour=0;
         }
         void SetVehicleType(int i)
         {
             switch(i)
             {
                 case 0://unknown vehicle
+                vtypeid=0;
                 this->type="Vehicle";
+                priceperhour=0;
                 break;
                 case 1://2 wheeler
+                vtypeid=1;
+                priceperhour=10;
                 this->type="2 wheeler";
                 break;
                 case 2://4 wheeler
+                vtypeid=2;
+                priceperhour=30;
                 this->type="4 wheeler";
                 break;
                 case 3://heavy vehicle
+                vtypeid=3;
+                priceperhour=50;
                 this->type="heavy vehicle";
                 break;
 
@@ -39,13 +61,13 @@ class VehicleType
 
 };
 
-class Vehicle
+class Vehicle:private VehicleType
 {
     private:
         string regid;
         tm *start;
         tm *end;
-        VehicleType vtype;
+        //VehicleType vtype;
 
         
     public:
@@ -60,7 +82,14 @@ class Vehicle
         this->regid="null";
         this->start=getTime();
         this->end=getTime();
-        this->vtype.SetVehicleType(0);
+        this->SetVehicleType(0);
+    }
+    Vehicle(string rid,int vt)
+    {
+        this->regid=rid;
+        this->start=getTime();
+        this->end=getTime();
+        this->SetVehicleType(vt);
     }
     void showVehicle()
     {
@@ -68,7 +97,7 @@ class Vehicle
         cout<< "Reg ID: "<<this->regid<<endl;
         cout<< "Arrival time: " << this->start->tm_hour << ":" << this->start->tm_min << ":" << this->start->tm_sec << endl;
         cout<< "Departure time: " << this->end->tm_hour << ":"<< this->end->tm_min << ":" << this->end->tm_sec << endl;
-        cout<< "Vehicle type: " <<this->vtype.showVehicleType() << endl;
+        cout<< "Vehicle type: " <<this->showVehicleType() << endl;
     }
         
 
@@ -77,7 +106,35 @@ class Vehicle
 
 int main()
 {
-    Vehicle v1;
-    v1.showVehicle();
+    int choice=99;
+    while(choice!=0)
+    {
+        cout<<"How may I help you ? | 1.Park the vehicle 2.Remove the vehicle from parking :"<<endl;
+        cin>>choice;
+        if(choice==1)
+        {
+            int vtype;
+            string regid;
+            cout<<"What type of vehicle is yours ? | 1. 2-Wheeler 2. 4-Wheeler 3. Heavy Vehicle"<<endl;
+            cin>>vtype;
+            cout<<"Enter Registration ID of the vehicle"<<endl;
+            cin>>regid;
+            Vehicle v(regid,vtype);
+
+            v.showVehicle();
+
+
+        }
+        else if(choice==2)
+        {
+
+        }
+        else{
+            cout<<"Invalid choice :("<<endl;
+
+        }
+        cout<<"Continue ? | 1.Yes 0. No :"<<endl;
+        cin>>choice;
+    }
     return 0;
 }
